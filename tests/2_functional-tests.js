fa8e6baca5812804bbdbc48fce569c9c5c932fd4
Diego Da'Silva <smartcrashomg@gmail.com>
2021-07-24 06:44:24 -0400

    Prettier

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,8 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
+const chaiHttp = require("chai-http");
+const chai = require("chai");
 let assert = chai.assert;
-const server = require('../server');
+const server = require("../server");
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-
-});
+suite("Functional Tests", function () {});
